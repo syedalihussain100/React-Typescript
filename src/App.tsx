87b904c5bbcb177ff3fd410c7815a56d72400650
syedalihussain100 <syedalihussain100@gmail.com>
2020-09-02 15:05:58 +0500

    React Typescript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello React Typescript!!</h2>
+      <input type="text" placeholder="Enter Your Value" />
+      <br /> <br />
+      <button>Submit Data!</button>
+
+    </div>
+  );
+}
+
+export default App;
